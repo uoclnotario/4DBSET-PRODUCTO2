e712f08c2b0771ce099ce1d3b71fd4d4a832d993
Emma Garcia <egarciasanti@uoc.edu>
2020-11-01 18:42:31 +0100

    Vista Delegacion

--- src/com/app/console/Vista/Delegaciones_vista.java
+++ src/com/app/console/Vista/Delegaciones_vista.java
@@ -1,15 +1,41 @@
 package com.app.console.Vista;
 
+import logicaEmpresarial.Delegacion;
 import logicaEmpresarial.Ong;
+import logicaEmpresarial.Personal;
 import logicaEmpresarial.Usuario;
-
+import javax.print.DocFlavor;
+import java.awt.desktop.SystemEventListener;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Optional;
+
 
 public class Delegaciones_vista implements Vista {
     @Override
     public String MostrarLIstado(List listado, String salir, Usuario user) {
-        return null;
+
+
+
+        if(listado == null){
+
+
+
+    }else{
+        List<Delegacion> delegacion = (List<Delegacion>)listado;
+        System.out.println("Listado de las delegaciones:");
+        System.out.println("\tNOMBRE\tDIRECCION");
+
+        for(int i = 0; i < delegacion.size();i++)
+            System.out.println("\t"+(i+1)+"\t"+delegacion.get(i).getNombre()+ "\t"+delegacion.get(i).getDireccion());
+
+
     }
+
+
+        return FuncionesConsola.leerConsola();
+}
 
     @Override
     public String MostrarUno(Object elemento, String salir, Usuario user) {
